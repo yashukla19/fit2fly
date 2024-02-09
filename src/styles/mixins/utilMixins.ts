f0ec69f288a3c6dd1987766c56eddc07f8ddd183
Yash <yashukla19@outlook.com>
2024-02-10 01:38:45 +0530

    Added mixins

---
+++ src/styles/mixins/utilMixins.ts
@@ -0,0 +1,17 @@
+const position = (
+    type: 'absolute' | 'relative' | 'fixed' | 'sticky',
+    top?: number | string,
+    right?: number | string,
+    bottom?: number | string,
+    left?: number | string,
+    zIndex?: number
+  ) => `
+      position: ${type};
+      ${top !== undefined ? `top: ${typeof top === 'number' ? `${top}px` : top};` : ''}
+      ${right !== undefined ? `right: ${typeof right === 'number' ? `${right}px` : right};` : ''}
+      ${bottom !== undefined ? `bottom: ${typeof bottom === 'number' ? `${bottom}px` : bottom};` : ''}
+      ${left !== undefined ? `left: ${typeof left === 'number' ? `${left}px` : left};` : ''}
+      ${zIndex !== undefined ? `z-index: ${zIndex};` : ''}
+  `;
+  
+  export { position };
